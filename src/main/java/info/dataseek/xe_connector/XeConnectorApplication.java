1fae075552324120eb83967402b423a99249a8d0
zhelun <zhelun.gai@tmacomms.com>
2019-06-14 17:32:46 +1000

    init commit

---
+++ src/main/java/info/dataseek/xe_connector/XeConnectorApplication.java
@@ -0,0 +1,13 @@
+package info.dataseek.xe_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XeConnectorApplication.class, args);
+    }
+
+}
