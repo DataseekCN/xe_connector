cc5856639877ab8856023c3ca2f7a600f9d1340e
zhelun <zhelun.gai@tmacomms.com>
2019-06-16 20:50:25 +1000

    add swagger integration

--- src/main/java/info/dataseek/xe_connector/controller/EmployeeController.java
+++ src/main/java/info/dataseek/xe_connector/controller/EmployeeController.java
@@ -2,14 +2,16 @@ package info.dataseek.xe_connector.controller;
 
 import info.dataseek.xe_connector.dataobject.EmployeeInfo;
 import info.dataseek.xe_connector.service.IEmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "EmployeeInfoController")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -17,6 +19,7 @@ public class EmployeeController {
     @Autowired
     private IEmployeeService employeeService;
 
+    @ApiOperation(value = "员工列表")
     @GetMapping("/list")
     public List<EmployeeInfo> getAllEmployee() {
         return employeeService.findAll();
